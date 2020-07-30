71622216314bcb803be20b123e128ef7b9828141
Aniket Jadhav <aniketjadhav71196@gmail.com>
2020-07-30 12:57:33 +0530

    Added two APIs for app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,9 @@
+//http://api.weatherstack.com/current?access_key=6096b2aa586e0bfb73d2271b39f16003&query=37.8267,-122.4233
+
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=6096b2aa586e0bfb73d2271b39f16003&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0]  + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
